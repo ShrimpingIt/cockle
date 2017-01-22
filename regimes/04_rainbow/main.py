6b623705f07bc1a0af5a8588feee3172e2ec5e5f
Cefn Hoile <github.com@cefn.com>
2017-01-22 14:54:39 +0000

    Added darken(). Fixed hueSeparation. Ensured clear is run at beginning. Made pixel.write() lazier.

--- regimes/04_rainbow/main.py
+++ regimes/04_rainbow/main.py
@@ -1,21 +1,26 @@
 from time import sleep
 from cockle import pins
 from ws2811 import *
+
 num_pixels = 8
 startPixels(pins[2], num_pixels)
-showPixels()
+clearPixels()
 
+hueOffset = 0
 allIndexes = range(num_pixels)
 hueSeparation = 1 / num_pixels
 hueChange = hueSeparation
-hueOffset = 0
 changeDelay = 1
+
+def darken(rgb, proportion=0.05):
+	return [int(value * proportion) for value in rgb]
 
 def rainbow():
     global hueOffset
     while True:
-        colors = [hue(((i * hueSeparation) + hueOffset) % 1) for i in allIndexes]
+        colors = [hue_to_rgb(((i * hueSeparation) + hueOffset) % 1) for i in allIndexes]
         for i in allIndexes:
-            setPixel(i, colors[i])
+            setPixel(i, colors[i], False)
+        showPixels()
         sleep(changeDelay)
         hueOffset = hueOffset + hueChange
