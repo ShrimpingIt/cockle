ec749e57d81c99e6ecfa01775c1b687ce4cdde98
Cefn Hoile <github.com@cefn.com>
2017-03-30 01:08:40 +0100

    Now tested

--- deployall.py
+++ deployall.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python
 from six.moves import input
 
+from config import putFile
+
+'''
 from flash.retrieve import run as flashRetrieve 
 from flash.deploy import run as flashDeploy 
 from modules.deploy import run as modulesDeploy
 from replserver.deploy import run as replserverDeploy 
 
+print("Retrieving latest flash image")
 flashRetrieve()
+print("Deploying flash image")
 flashDeploy()
+
 input('Micropython uploaded. Press Cockle reset button, then press Enter')
+
+print("Deploying Modules")
 modulesDeploy()
+print("Deploying REPLServer")
 replserverDeploy()
-config.putFile("./regimes/04_rainbow/main.py", "main.py")
-input('All modules, boot and main files deployed. Press Cockle reset button, then press Enter')
+'''
+print("Deploying Application main.py file")
+putFile("./regimes/04_rainbow/main.py", "main.py")
+
+input('All modules, replserver boot and application main files deployed. Press Cockle reset button, then press Enter')
