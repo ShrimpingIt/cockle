24b1da3ef9b277fd2fea8b712253adaffb5572d6
Cefn Hoile <github.com@cefn.com>
2017-04-03 08:34:26 +0100

    Added some reporting about download status

--- flash/retrieve.py
+++ flash/retrieve.py
@@ -10,10 +10,13 @@ def get_firmware():
 	local_path = retrievalLookup["local_image_path"]
 	r = requests.get(remote_url, stream=True)
 	if not(os.path.isfile(local_path)):
+		print("Retrieving Micropython Image")
 		with open(local_path, 'wb') as f:
 			for chunk in r.iter_content(chunk_size=1024): 
 				if chunk:
 					f.write(chunk)
+	else:
+		print("Micropython image already retrieved")
 	return local_path
 	
 def run():
