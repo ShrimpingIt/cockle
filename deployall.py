1d593448f6f623bc3c0eb779951612d9b76715e3
Cefn Hoile <github.com@cefn.com>
2017-07-25 18:34:00 +0100

    No main by default. Extra blank lines

--- deployall.py
+++ deployall.py
@@ -1,42 +1,47 @@
 #!/usr/bin/python
-mainPath = "./regimes/04_rainbow/main.py"
+#mainPath = "./regimes/04_rainbow/main.py"
+mainPath = None
 
 from time import sleep
 from six.moves import input
 
-from config import putFile,resetBoard
+from config import putFile, resetBoard
 
-from flash.retrieve import run as flashRetrieve 
-from flash.deploy import run as flashDeploy 
+from flash.retrieve import run as flashRetrieve
+from flash.deploy import run as flashDeploy
 from modules.deploy import run as modulesDeploy
-from replserver.deploy import run as replserverDeploy 
+from replserver.deploy import run as replserverDeploy
+
 
 def runFlash():
 
-	print("Checking Micropython image retrieved")
-	flashRetrieve()
+    print("Checking Micropython image retrieved")
+    flashRetrieve()
 
-	print("Deploying Micropython image")
-	flashDeploy()
-	
-	sleep(4)
+    print("Deploying Micropython image")
+    flashDeploy()
+
+    sleep(4)
+
 
 def runInstall():
-	
-	print("Deploying Modules")
-	modulesDeploy()
 
-	print("Deploying REPLServer")
-	replserverDeploy()
+    print("Deploying Modules")
+    modulesDeploy()
 
-	print("Deploying Application main.py file")
-	putFile(mainPath, "main.py")
+    print("Deploying REPLServer")
+    replserverDeploy()
+
+    if mainPath is not None:
+        print("Deploying Application main.py file")
+        putFile(mainPath, "main.py")
 
 
 def run():
-	runFlash()
-	runInstall()
-	print('All modules, replserver boot and application main files deployed. Now unplug the cockle to ensure it resets \a\a\a')
+    runFlash()
+    runInstall()
+    print('All modules, replserver boot and application main files deployed. Now unplug the cockle to ensure it resets \a\a\a')
+
 
 if __name__ == "__main__":
-	run()
+    run()
