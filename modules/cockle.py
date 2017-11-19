79d6816a4c205783ed6bcce57c51320f787391a9
Cefn Hoile <github.com@cefn.com>
2017-11-19 18:56:07 +0000

    Added reference http_get implementation from Micropython website

--- modules/cockle.py
+++ modules/cockle.py
@@ -5,7 +5,6 @@ from utime import sleep_ms, ticks_ms, ticks_diff
 
 uplink = None
 
-
 pins = [
     Pin(16), #D0
     Pin(5),  #D1 PWM
@@ -41,6 +40,8 @@ def https_get(url, headers=None):
     _, _, host, path = url.split('/', 3)
     import usocket
     import ussl
+    import sys
+    import gc
     addr = usocket.getaddrinfo(host, 443)[0][-1]
     s=usocket.socket()
     s.connect(addr)
@@ -62,13 +63,37 @@ def https_get(url, headers=None):
                     else:
                         sys.stdout.write(buf)
                     continue
-                else:
-                    print("Count not greater than 0")
             except OSError as ose:
                 print(ose)
             break
     finally:
         s.close()
+
+def http_get(url):
+    _, _, host, path = url.split('/', 3)
+    import usocket
+    import gc
+    import sys
+    addr = usocket.getaddrinfo(host, 80)[0][-1]
+    s = usocket.socket()
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+
+    buf = bytearray(128)
+    while True:
+        gc.collect()
+        try:
+            count = s.readinto(buf) # TODO use readline for headers including e.g. 'content-length: 2358' then after blank line, count bytes before close
+            if count > 0:
+                if count < len(buf):
+                    sys.stdout.write(buf[:count])
+                else:
+                    sys.stdout.write(buf)
+                continue
+        except OSError as ose:
+            print(ose)
+        break
+    s.close()
 
 def mac():      
     from network import WLAN
